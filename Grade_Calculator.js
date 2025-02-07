bd5e8fbec9924bc67f5a6e7c9c16812e97f3a51e
Shafi031 <shohanurrahman163@outlook.com>
2025-02-07 11:32:41 +0600

    Gradee

---
+++ Grade_Calculator.js
@@ -0,0 +1,17 @@
+let score=50;
+
+if(score<=100 && score>=90){
+    console.log("A");
+}
+else if(score>=80){
+    console.log("B");
+}
+else if(score>=70){
+    console.log("B");
+}
+else if(score>=60){
+    console.log("B");
+}
+else{
+    console.log("F");
+}
